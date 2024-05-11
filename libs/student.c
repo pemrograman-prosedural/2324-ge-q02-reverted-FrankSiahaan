9f69b2fd859c64f6cb69035d0f3e419c4e2a3d58
FrankSiahaan <frankniroysiahaan1@gmail.com>
2024-05-11 10:39:29 +0700

    q02

--- libs/student.c
+++ libs/student.c
@@ -1,7 +1,93 @@
 #include "student.h"
+#include <string.h>
+#include <stdio.h>
 
 /**
  * @brief Define the complete function definition here. Be sure to enlist the prototype of each function
  * defined here in the corresponding header file.
  *
  */
+struct student_t create_student(char *_id, char *_name, char *_year, enum gender_t _gender)
+{
+  struct student_t std;
+  strcpy(std.id, _id);
+  strcpy(std.name, _name);
+  strcpy(std.year, _year);
+  std.gender = _gender;
+  std.dorm = NULL;
+
+  return std;
+}
+
+void print_student(struct student_t *_student, int jumlah)
+{
+    for (int i = 0; i < jumlah; i++)
+    {
+        if (_student[i].gender == 0)
+        {
+            printf("%s|%s|%s|male\n", _student[i].id, _student[i].name, _student[i].year);
+        }else {
+            printf("%s|%s|%s|female\n", _student[i].id, _student[i].name, _student[i].year);
+        }
+        
+    }
+}
+
+void print_student_detail(struct student_t *_student, int jumlah){
+    for (int i = 0; i < jumlah; i++)
+    {
+        if (_student[i].dorm == NULL)
+        {
+            if (_student[i].gender == 0)
+        {
+            printf("%s|%s|%s|male|unassigned\n", _student[i].id, _student[i].name, _student[i].year);
+        }else {
+            printf("%s|%s|%s|female|unassigned\n", _student[i].id, _student[i].name, _student[i].year);
+        }
+        }else {
+            if (_student[i].gender == 0)
+        {
+            printf("%s|%s|%s|male|%s\n", _student[i].id, _student[i].name, _student[i].year, _student[i].dorm->name);
+        }else {
+            printf("%s|%s|%s|female|%s\n", _student[i].id, _student[i].name, _student[i].year, _student[i].dorm->name);
+        }
+        } 
+    }
+}
+
+void assign_student(struct student_t *_student, struct dorm_t *_dorm, char *id, char *names) {
+    if (_dorm->gender == _student->gender)
+    {
+        if (_dorm->capacity > _dorm->residents_num)
+        {
+            _student->dorm = _dorm;
+            _dorm->residents_num++;
+        } else {
+            _student->dorm = NULL;
+        }
+        
+    }
+    
+}
+
+void move_student(struct student_t *_student, struct dorm_t *new_dorm ,struct dorm_t *old_dorm, int dorm_index, int oldindex, int student_index) {
+  if (new_dorm->gender == _student->gender)
+    {
+        if (new_dorm->capacity > new_dorm->residents_num)
+        {
+            new_dorm[dorm_index].residents_num++;
+            strcpy(_student[student_index].dorm->name, new_dorm->name);
+            old_dorm[oldindex].residents_num--;
+        }   
+        
+    }
+}
+void dorm_empty(struct student_t *_student, struct dorm_t *_dorm, int jumlah, char *names1, int student_index){
+    for (int i = 0; i < student_index; i++)
+    {
+        strcpy(_student[i].dorm->name, "unassigned");
+        _dorm[i].residents_num--;        
+    }
+    
+}
+
--- q02_01.c
+++ q02_01.c
@@ -1,12 +1,188 @@
-// NIM - Name
-// NIM - Name
-
+// 12S23016 - Frank Niroy Siahaan
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "./libs/dorm.h"
 #include "./libs/student.h"
 
 int main(int _argc, char **_argv)
 {
-    // codes
+    struct student_t *students = malloc(50 * sizeof(struct student_t));
+    struct dorm_t *dorms = malloc(50 * sizeof(struct dorm_t));
+    char cmd[255];
+    char id[12];
+    char name[40];
+    char year[5];
+    char names[200];
+    char *token;
+    unsigned short capacity;
+    int i = 0; 
+    int j = 0;
+    int student_index = -1;
+    int dorm_index = -1;
+    int oldindex = 0;
+    char names1[200];
+    int dorm_index1 = 0;
+    
+
+    while (1)
+    {
+        fgets(cmd, sizeof(cmd), stdin);
+        
+      
+        if (cmd[strlen(cmd) - 1] == '\n' || cmd[strlen(cmd) - 1] == '\r')
+        {
+            cmd[strlen(cmd) - 1] = '\0';
+        }
+
+        if (strcmp(cmd, "---") == 0)
+        {
+            break;
+        }
+        else
+        {
+            token = strtok(cmd, "#");
+            if (strcmp(token, "student-add") == 0)
+            {
+                token = strtok(NULL, "#");
+                strcpy(id, token);
+
+                token = strtok(NULL, "#");
+                strcpy(name, token);
+
+                token = strtok(NULL, "#");
+                strcpy(year, token);
+
+                token = strtok(NULL, "#");
+                if (strcmp(token, "male") == 0)
+                {
+                    students[i] = create_student(id, name, year, GENDER_MALE);
+                }
+                else if (strcmp(token, "female") == 0)
+                {
+                    students[i] = create_student(id, name, year, GENDER_FEMALE);
+                }
+                i++;
+            }
+            else if (strcmp(token, "student-print-all") == 0)
+            {
+                void (*pf) (struct student_t *student, int y);
+                pf = print_student;
+                pf(students, i);
+            } 
+            else if (strcmp(token, "student-print-all-detail") == 0) {
+                void (*pf) (struct student_t *student, int y);
+                pf = print_student_detail;
+                pf(students, i);
+            }
+            else if (strcmp(token, "dorm-print-all-detail") == 0) {
+                void (*pf) (struct dorm_t *dorm, int y);
+                pf = print_dorm_detail;
+                pf(dorms, j);
+            }
+            else if (strcmp(token, "dorm-add") == 0) {
+                token = strtok(NULL, "#");
+                strcpy(name, token);
+
+                token = strtok(NULL, "#");
+                capacity = atoi(token);
+
+                token = strtok(NULL, "#");
+                 if (strcmp(token, "male") == 0)
+                {
+                    dorms[j] = create_dorm(name, capacity, GENDER_MALE);
+                }
+                else if (strcmp(token, "female") == 0)
+                {
+                    dorms[j] = create_dorm(name, capacity, GENDER_FEMALE);
+                }
+                j++;
+            }
+            else if (strcmp(token, "dorm-print-all") == 0) {
+                void (*pf) (struct dorm_t *dorm, int y);
+                pf = print_dorm;
+                pf(dorms, j);
+            }
+            else if (strcmp(token, "assign-student") == 0) {
+                token = strtok(NULL, "#");
+                strcpy(id, token);
+
+                token = strtok(NULL, "#");
+                strcpy(names, token);
+
+                for (int s = 0; s < i; ++s)
+                {
+                    if (strcmp(students[s].id, id) == 0)
+                    {
+                        student_index = s;      
+                        break; 
+                    }
+                }   
+                for (int d = 0; d < i; ++d)
+                {
+                    if (strcmp(dorms[d].name, names) == 0)
+                    {
+                        dorm_index = d;    
+                        break;   
+                    }
+                }   
+                assign_student(&students[student_index], &dorms[dorm_index], id, names);
+            } else if (strcmp(token, "move-student") == 0) {
+                token = strtok(NULL, "#");
+                strcpy(id, token);
+
+                token = strtok(NULL, "#");
+                strcpy(names, token);
+
+                for (int g = 0; g < i; g++)
+                {
+                    if (strcmp(id, students[g].id) == 0)
+                    {
+                        student_index = g;
+                        break;
+                    }
+                }
+                for (int d = 0; d < j; d++)
+                {
+                    if (strcmp(names, dorms[d].name) == 0)
+                    {
+                        dorm_index = d;
+                        break;
+                    }
+                }
+                if (students[student_index].dorm == NULL)
+                {
+                    assign_student(&students[student_index], &dorms[dorm_index], id, names);
+                } else {
+                    for (int b = 0; b <= j; b++)
+                    {
+                        if (strcmp(students[b].dorm->name, dorms[b].name) == 0)
+                        {
+                            oldindex = b;
+                            break;
+                        }
+                        move_student(&students[student_index], &dorms[dorm_index] ,&dorms[oldindex], dorm_index, oldindex, student_index);
+
+                    }                    
+                }
+            } else if (strcmp(token, "dorm-empty") == 0) {
+                token = strtok(NULL, "#");
+                strcpy(names1, token);
+
+                for (int x = 0; x < j; x++)
+                {
+                    if (strcmp(dorms[x].name, names1) == 0)
+                    {
+                        dorm_index1 = x;
+                        break;
+                    }
+                }
+                dorm_empty(&students[student_index], &dorms[dorm_index1], dorm_index1, names1, student_index);
+            }
+        }
+    }
+
+    free(students);
+    free(dorms);
     return 0;
 }
